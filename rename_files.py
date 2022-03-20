a4abd20cb5fe88ad7da1cd3453901a0fb82b6029
Crazydud3gam3r <tardistestpilot@gmail.com>
2022-03-19 23:56:17 -0400

    naming scheme works, making look better l8er

--- rename_files.py
+++ rename_files.py
@@ -8,10 +8,15 @@ NAME = ""
 FILE_EXTENSION = ".mkv"
 TOTAL_EPISODES = 0
 NAMING_SCHEME = ""
+EP_PER_SEASON = 0
 
-def rename_files(directory,name,file_extension,total_episodes):
+def rename_files(directory,name,file_extension,total_episodes,scheme):
     episode = 1
+    season = 1
     for filename in os.listdir(directory):
+        if episode > EP_PER_SEASON:
+            season += 1
+            episode = 1
         EP = str(episode)
         source = directory + filename
         if total_episodes > 10 and not (total_episodes > 100):
@@ -29,7 +34,15 @@ def rename_files(directory,name,file_extension,total_episodes):
                 EP = "00" + EP
             elif episode <= 999:
                 EP = "0" + EP
-        destination = directory + name + " Episode " + EP + file_extension
+        match scheme:
+            case "<Name> Episode #":
+                destination = directory + name + " Episode " + EP + file_extension
+            case "<Name> Season # Episode #":
+                destination = directory + name + " Season " + str(season) + " Episode " + EP + file_extension
+            case "<Name> E#":
+                destination = directory + name + " E" + EP + file_extension
+            case "<Name> S#E#":
+                destination = directory + name + " S" + str(season) + "E" + EP + file_extension
         os.rename(source, destination)
         episode += 1
 
@@ -67,20 +80,20 @@ def gui():
     total_entry.insert(0,"total number of episodes")
     total_entry.pack(padx=5,pady=5)
     scheme_list = ["<Name> Episode #" , "<Name> Season # Episode #", "<Name> E#","<Name> S#E#"]
-    scheme_chooser = ttk.Combobox(frame, values = scheme_list, width=42)
-    scheme_chooser.set("Select the naming scheme you want to use")
-    scheme_chooser.pack(pady = 10)
+    scheme_choice = ttk.Combobox(frame, values = scheme_list, width=42)
+    scheme_choice.set("Select the naming scheme you want to use")
+    scheme_choice.pack(pady = 10)
     season_text = StringVar()
-    season_text.set("Enter how many episodes there are per season:\n(leave this option at 0 if it is not needed)")
+    season_text.set("Enter how many episodes there are per season:\n(leave this option at 9999 if it is not needed)")
     season_label = Label(frame, textvariable = season_text)
     season_label.pack()
     season_entry = Entry(frame, width = 45,bd=2,)
-    season_entry.insert(0,"0")
+    season_entry.insert(0,"9999")
     season_entry.pack(padx=5,pady=5)
     bottom_frame = Frame(base)
     bottom_frame.pack(side=BOTTOM)
-    run_button = Button(bottom_frame, text = "Rename Files!", command=lambda:run_all(dir_entry,name_entry,ext_entry,total_entry), 
-    font = ("Impact 15"))
+    run_button = Button(bottom_frame, text = "Rename Files!", command=lambda:run_all(dir_entry,name_entry,ext_entry,total_entry,scheme_choice, 
+    season_entry), font = ("Impact 15"))
     run_button.pack(pady=5)
     base.title("Batch Rename")
     base.mainloop()
@@ -106,13 +119,18 @@ def set_naming(combo:ttk.Combobox):
     global NAMING_SCHEME
     NAMING_SCHEME = combo.get()
 
-def run_all(dir:Entry,name:Entry,ext:Entry,total:Entry,scheme:ttk.Combobox):
+def set_season(entry:Entry):
+    global EP_PER_SEASON
+    EP_PER_SEASON = int(entry.get())
+
+def run_all(dir:Entry,name:Entry,ext:Entry,total:Entry,scheme:ttk.Combobox,season:Entry):
     set_dir(dir)
     set_name(name)
     set_ext(ext)
     set_total(total)
     set_naming(scheme)
-    rename_files(DIRECTORY,NAME,FILE_EXTENSION,TOTAL_EPISODES)
+    set_season(season)
+    rename_files(DIRECTORY,NAME,FILE_EXTENSION,TOTAL_EPISODES,NAMING_SCHEME)
 
 if __name__ == "__main__":
     gui()
