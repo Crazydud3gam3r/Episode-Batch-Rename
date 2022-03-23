50243c29511919559b544a273a749ee955551601
Crazydud3gam3r <89417394+Crazydud3gam3r@users.noreply.github.com>
2022-03-23 17:30:58 -0400

    Fixed the issue, I was stupid

--- rename_files.py
+++ rename_files.py
@@ -30,13 +30,13 @@ def rename_files(directory,name,file_extension,total_episodes,scheme,starting_ep
             elif episode <= 999:
                 EP = "0" + EP
         match scheme:
-            case "<Name> Episode #":
+            case "<Name> - Episode #":
                 destination = directory + name + " - Episode " + EP + file_extension
-            case "<Name> Season # Episode #":
+            case "<Name> - Season # Episode #":
                 destination = directory + name + " - Season " + str(season) + " - Episode " + EP + file_extension
-            case "<Name> E#":
+            case "<Name> - E#":
                 destination = directory + name + " - E" + EP + file_extension
-            case "<Name> S#E#":
+            case "<Name> - S#E#":
                 destination = directory + name + " - S" + str(season) + "E" + EP + file_extension
         os.rename(source, destination)
         episode += 1
