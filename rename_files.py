83f711908a692e08e10b0fcf3dbdbc05963c7c36
Crazydud3gam3r <tardistestpilot@gmail.com>
2022-03-23 16:28:24 -0400

    made it better, fixing more after my class

--- rename_files.py
+++ rename_files.py
@@ -7,7 +7,9 @@ def rename_files(directory,name,file_extension,total_episodes,ep_per_season,sche
     episode = 1
     season = 1
     for filename in os.listdir(directory):
-        if episode > ep_per_season:
+        if ep_per_season < 10:
+            season = ep_per_season
+        elif episode > ep_per_season:
             season += 1
             episode = 1
         EP = str(episode)
@@ -15,7 +17,7 @@ def rename_files(directory,name,file_extension,total_episodes,ep_per_season,sche
         if total_episodes > 10 and not (total_episodes > 100):
             if episode <= 9:
                 EP = "0" + EP
-        elif total_episodes > 100 and not (total_episodes > 1000):
+        elif total_episodes >= 100 and not (total_episodes > 1000):
             if episode <= 9:
                 EP = "00" + EP
             elif episode <= 99:
@@ -42,7 +44,7 @@ def rename_files(directory,name,file_extension,total_episodes,ep_per_season,sche
 def gui():
     e_list = []
     base = Tk()
-    base.geometry("300x370")
+    base.geometry("300x450")
     frame = Frame(base)
     frame.pack()
     dir_label_text = "Enter the path to the folder containing the files:"
@@ -53,17 +55,20 @@ def gui():
     ext_entry_text = ".mkv"
     total_label_text = "Enter the total number of episodes:"
     total_entry_text = "total number of episodes"
-    season_label_text = "Enter how many episodes there are per season:\n(leave this option at 9999 if it is not needed)"
+    season_label_text = "Enter how many episodes there are per season:\nSet the season number by using a number < 10\nleave at 1 if unneeded"
     season_entry_text = "9999"
+    starting_label_text = "Enter the starting episode number:\n(leave at 1 if unneeded)"
+    starting_entry_text = "1"
     label_entry_pack(dir_label_text,dir_entry_text,frame,e_list)
     label_entry_pack(name_label_text,name_entry_text,frame,e_list)
     label_entry_pack(ext_label_text,ext_entry_text,frame,e_list)
     label_entry_pack(total_label_text,total_entry_text,frame,e_list)
+    label_entry_pack(starting_label_text,starting_entry_text,frame,e_list)
+    label_entry_pack(season_label_text,season_entry_text,frame,e_list)
     scheme_list = ["<Name> Episode #" , "<Name> Season # Episode #", "<Name> E#","<Name> S#E#"]
     scheme_choice = ttk.Combobox(frame, values = scheme_list, width=42)
     scheme_choice.set("Select the naming scheme you want to use")
     scheme_choice.pack(pady = 10)
-    label_entry_pack(season_label_text,season_entry_text,frame,e_list)
     bottom_frame = Frame(base)
     bottom_frame.pack(side=BOTTOM)
     run_button = Button(bottom_frame, text = "Rename Files!", command=lambda:run_all(e_list[0],e_list[1],e_list[2],e_list[3],e_list[4], 
